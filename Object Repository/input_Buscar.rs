1cd93b9e3afcb9a8550472f5c1731402bdcabd6b
Jair <jairbs98@gmail.com>
2022-12-09 12:46:03 -0700

    Nuevo caso de pruebas y objetos

---
+++ Object Repository/input_Buscar.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>//input[contains(@class,'gLFyf')]</description>
+   <name>input_Buscar</name>
+   <tag></tag>
+   <elementGuidId>4b195e0e-b50a-4a57-b353-a17c140f61a2</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[contains(@class,'gLFyf')]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/span_Resultado.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>//span[@class='qv3Wpe' and @id='cwos']</description>
+   <name>span_Resultado</name>
+   <tag></tag>
+   <elementGuidId>b26d9cdb-cba9-444b-ba11-1f0a4931f087</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[@class='qv3Wpe' and @id='cwos']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
